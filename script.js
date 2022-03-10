e6eb9bd1aa208bce79a7b16be62cebd415ff8752
AmberPhuongNguyen <amberphuongnguyen020993@gmail.com>
2022-03-10 22:57:53 +0700

    fetching 'n' displaying jokes

--- script.js
+++ script.js
@@ -5,16 +5,31 @@ jokeBtn.addEventListener('click', generateJokes);
 
 generateJokes();
 
-function generateJokes() {
+
+//ASYNC/AWAIT Method
+async function generateJokes() {
     const config = {
         headers: {
             'Accept': 'application/json'
         }
     }
-    fetch('https://icanhazdadjoke.com', config)
-        //then syntax
-        .then((res) => res.json())
-        .then((data) => {
-            jokeElement.innerHTML = data.joke
-        })
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json();
+
+    jokeElement.innerHTML = data.joke
 }
+
+//DOT THEN method
+// function generateJokes() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//         //then syntax
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeElement.innerHTML = data.joke
+//         })
+// }
